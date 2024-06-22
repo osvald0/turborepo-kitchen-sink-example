79775ccbbb90efa818850a9593c9e1be7bc4c483
Osvaldo Colina <osvaldo@crowdvolt.com>
2024-06-21 21:43:32 -0300

    first commit

---
+++ apps/blog/app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "@repo/ui/link";
+import { CounterButton } from "@repo/ui/counter-button";
+
+export default function Index(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Blog <br />
+        <span>Kitchen Sink</span>
+      </h1>
+      <CounterButton />
+      <p className="description">
+        Built With <Link href="https://turbo.build/repo">Turborepo</Link>
+        {" & "}
+        <Link href="https://remix.run/">Remix</Link>
+      </p>
+    </div>
+  );
+}
